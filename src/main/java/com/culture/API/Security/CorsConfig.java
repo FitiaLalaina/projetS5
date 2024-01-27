83367fbb8bc7773c9607aec2184e61d7683058fd
FitiaLalaina <124667098+FitiaLalaina@users.noreply.github.com>
2024-01-27 13:36:17 +0300

    Update CorsConfig.java

--- src/main/java/com/culture/API/Security/CorsConfig.java
+++ src/main/java/com/culture/API/Security/CorsConfig.java
@@ -19,7 +19,7 @@ public class CorsConfig {
                 registry.addMapping("/api/**")
                 .allowedMethods(CorsConfiguration.ALL)
                 .allowedHeaders(CorsConfiguration.ALL)
-                .allowed    Origins(CorsConfiguration.ALL);
+                .allowedOrigins(CorsConfiguration.ALL);
             }
         };
     }
